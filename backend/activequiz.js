8e2ce99d3236b438b7972643d0fb0e9d98dbeb14
Abdullah Azeem <abdullahazeem770@gmail.com>
2024-01-14 00:15:10 +0530

    gitignore added

--- backend/activequiz.js
+++ backend/activequiz.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 dotenv.config();
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import  question  from './quiz.model.js';
-import connectdb from './db.js';
-import mongoose from 'mongoose';
-import { user } from './user.model.js';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import question from "./quiz.model.js";
+import connectdb from "./db.js";
+import mongoose from "mongoose";
+import { user } from "./user.model.js";
 connectdb();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -18,15 +18,15 @@ const port = process.env.port;
 app.use(bodyParser.json());
 
 app.get("/activequiz", async (req, res) => {
-    const quizzes = await question.find({}, "quizName");
-    const name = [];
-    quizzes.map((Element) => {
-      name.push(Element.quizName);
-      console.log(Element.quizName);
-    });
-    res.send(name);
+  const quizzes = await question.find({}, "quizName");
+  const name = [];
+  quizzes.map((Element) => {
+    name.push(Element.quizName);
+    console.log(Element.quizName);
+  });
+  res.send(name);
 });
 
-app.listen(3000,()=>{
-    console.log("running at port 3000");
-})
+app.listen(3000, () => {
+  console.log("running at port 3000");
+});
