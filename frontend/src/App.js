2b0b801e09c1305b06d5b8f5047530362521cba4
raj210809 <145861977+raj210809@users.noreply.github.com>
2024-01-14 02:27:09 +0530

    modified:   frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Home from "./components/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Webpage1 from "./components/Webpage1.js";
 import { Route, Routes } from "react-router-dom";
+import Webpage2 from "./components/Webpage2.js";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route exact path="" element={<Home />} />
         <Route exact path="About" element={<Webpage1 />} />
+        <Route exact path="Contact" element={<Webpage2 />} />
       </Routes>
     </>
   );
